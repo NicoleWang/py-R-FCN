bd237a69553d96823db9e95196d73d1653612c7e
NicoleWang <434707665@qq.com>
2017-01-22 15:09:15 +0800

    create branch gray-rfcn

--- lib/datasets/text.py
+++ lib/datasets/text.py
@@ -64,7 +64,7 @@ class text(imdb):
         """
         Construct an image path from the image's "index" identifier.
         """
-        image_path = os.path.join(self._data_path, 'JPEGImages', index)
+        image_path = os.path.join(self._data_path, 'GrayImages', index)
         assert os.path.exists(image_path), \
                 'Path does not exist: {}'.format(image_path)
         return image_path
