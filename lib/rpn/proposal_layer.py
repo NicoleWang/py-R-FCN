523afd57bfe3af654d8b04e41b26ecb2a5e553fe
tiger <tiger@n9-018-146.byted.org>
2017-03-03 16:09:14 +0800

    set scales to 1 2 4

--- lib/rpn/proposal_layer.py
+++ lib/rpn/proposal_layer.py
@@ -27,7 +27,7 @@ class ProposalLayer(caffe.Layer):
 
         self._feat_stride = layer_params['feat_stride']
         #anchor_scales = layer_params.get('scales', (8, 16, 32))#original
-        anchor_scales = layer_params.get('scales', (2, 4, 8))
+        anchor_scales = layer_params.get('scales', (1, 2, 4))
         self._anchors = generate_anchors(scales=np.array(anchor_scales))
         self._num_anchors = self._anchors.shape[0]
 
